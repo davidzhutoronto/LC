ad41f74fc68351658581286bedf4741d6d8d1b54
David Zhu <dzhu34@myseneca.ca>
2023-04-05 14:04:09 -0400

    Create solution.js

---
+++ 977_Squares_of_a_Sorted_Array/solution.js
@@ -0,0 +1,50 @@
+/* Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+ 
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach? */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let myArray = [];
+  if (nums[0] >= 0) {
+    for (let i in nums) {
+      myArray.push(nums[i] ** 2);
+    }
+  } else {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+      if (nums[left] * nums[left] >= nums[right] * nums[right]) {
+        myArray.unshift(nums[left] * nums[left]);
+        left++;
+      } else {
+        myArray.unshift(nums[right] * nums[right]);
+        right--;
+      }
+    }
+  }
+
+  return myArray;
+};

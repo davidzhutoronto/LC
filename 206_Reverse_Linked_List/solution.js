b3fa11be007638852f7bd15150139935f08ccd33
David Zhu <dzhu34@myseneca.ca>
2023-04-05 12:28:48 -0400

    Create solution.js

---
+++ 206_Reverse_Linked_List/solution.js
@@ -0,0 +1,45 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: [] */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let myList = head;
+  let temp = null;
+  let prev = null;
+
+  if (head === null || head.next === null) return head;
+
+  while (myList) {
+    temp = myList.next;
+    myList.next = prev;
+    prev = myList;
+    myList = temp;
+  }
+  return prev;
+};
